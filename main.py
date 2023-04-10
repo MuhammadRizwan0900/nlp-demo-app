af621ded406d0d8c764730fa29a3857f866adaa9
rizwanahmed <rizvenahmed@gmail.com>
2023-04-10 15:41:33 +0500

    applied nlp techniques on data

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from tokenization import tokenize_text
 
-
-sentences, words = tokenize_text()
-tokenizationOutput = open('tokenization-output.txt', 'w+')
-tokenizationOutput.write("Sentences output:")
-tokenizationOutput.write(str(sentences))
-tokenizationOutput.write("\n")
-tokenizationOutput.write("Words output:")
-tokenizationOutput.write(str(words))
-print("Sentences: ", sentences)
-print("Words: ", words)
-
+tokenize_text()
+# tokenizationOutput = open('tokenization-output.txt', 'w+')
+# tokenizationOutput.write("Sentences output:")
+# tokenizationOutput.write(str(sentences))
+# tokenizationOutput.write("\n")
+# tokenizationOutput.write("Words output:")
+# tokenizationOutput.write(str(words))
+# print("Sentences: ", sentences)
+# print("Words: ", words)
+#Perform these steps
+#Tokenize the text into sentences.
+#Tokenize each sentence into words.
+#Convert all the text to lowercase.
+#Remove all punctuation marks and special characters.
+#Remove stop words (such as "the," "a," "an," "and," etc.) to reduce noise in the data.
+#Perform stemming or lemmatization to reduce words to their root form
+# import openai
+# openai.api_key = "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"
+#
+# response = openai.Completion.create(
+#     model="curie:ft-personal-2023-04-10-07-04-48",
+#     prompt="shipping")
+# print('resp:::', response.choices[0].text)
--- tokenization.py
+++ tokenization.py
@@ -1,14 +1,46 @@
 import nltk
 # nltk.download('punkt')
 from nltk.tokenize import word_tokenize, sent_tokenize
+import re
+from nltk.corpus import stopwords
+import string
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+# nltk.download('wordnet')
+# nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
 text = open("data.txt").read()
-print(text)
+
+
 def tokenize_text():
-    # Tokenize the text into sentences
     sentences = sent_tokenize(text)
+    print('sentences::', sentences)
+    words = []
+    for sentence in sentences:
+        words.extend(word_tokenize(sentence))
 
-    # Tokenize each sentence into words
-    words = [word_tokenize(sentence) for sentence in sentences]
+    print('words:::::', words)
+    words = [word.lower() for word in words]
 
-    # Return the tokenized text
-    return sentences, words
+    print(words)
+    # punctuation remove
+    words = [word for word in words if word not in string.punctuation]
+
+    print('punctuation::', words)
+    # Stop word remove
+    words = [word for word in words if word not in stop_words]
+
+    print('stop_words::', words)
+
+    stemmer = PorterStemmer()
+
+    stemmed_words = [stemmer.stem(word) for word in words]
+
+    print('stemmed_words::', stemmed_words)
+
+    legitimatize = WordNetLemmatizer()
+
+    lemmatized_words = [legitimatize.lemmatize(word) for word in words]
+
+    print('lemmatized_words::', lemmatized_words)
